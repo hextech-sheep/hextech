a0f4c3148dd8634f798a10996a9fed8d320990d2
James Glenn <jglenn1@gmail.com>
2018-10-19 15:07:34 -0500

    [WIP] add region/summoner name parameters to leagueverify command

--- hextech-gateway/src/main/java/com/hextechsheep/hextech/gateway/CommandLeagueVerify.java
+++ hextech-gateway/src/main/java/com/hextechsheep/hextech/gateway/CommandLeagueVerify.java
@@ -7,6 +7,7 @@ import org.bukkit.entity.Player;
 
 import com.merakianalytics.orianna.types.common.Region;
 
+import java.util.Arrays;
 import java.util.UUID;
 
 public class CommandLeagueVerify implements CommandExecutor {
@@ -22,7 +23,23 @@ public class CommandLeagueVerify implements CommandExecutor {
             if (playerId == null) {
                 return false;
             }
-            boolean verified = leagueIdentityProvider.verifyToken(playerId, "WxWatch", Region.NORTH_AMERICA);
+
+            if (args.length == 0 && args.length == 1) {
+                return false;
+            }
+
+            String summonerName = String.join(" ", Arrays.copyOfRange(args, 0, args.length - 1));
+            if (summonerName == null) {
+                return false;
+            }
+
+            String regionString = args[args.length - 1];
+            Region region = getRegion(regionString);
+            if (region == null) {
+                return false;
+            }
+
+            boolean verified = leagueIdentityProvider.verifyToken(playerId, summonerName, region);
             if (verified) {
                 player.sendMessage("Successfully verified!");
             } else {
@@ -31,5 +48,34 @@ public class CommandLeagueVerify implements CommandExecutor {
         }
         
         return true;
+    }
+
+    private Region getRegion(String shortname) {
+        switch (shortname.toLowerCase()) {
+            case "br":
+                return Region.BRAZIL;
+            case "eune":
+                return Region.EUROPE_NORTH_EAST;
+            case "euw":
+                return Region.EUROPE_WEST;
+            case "jp":
+                return Region.JAPAN;
+            case "kr":
+                return Region.KOREA;
+            case "lan":
+                return Region.LATIN_AMERICA_NORTH;
+            case "las":
+                return Region.LATIN_AMERICA_SOUTH;
+            case "na":
+                return Region.NORTH_AMERICA;
+            case "oce":
+                return Region.OCEANIA;
+            case "ru":
+                return Region.RUSSIA;
+            case "tr":
+                return Region.TURKEY;
+            default:
+                return null;
+        }
     }
 }
